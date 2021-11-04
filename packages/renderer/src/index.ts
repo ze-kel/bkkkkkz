f27c8993e5d0a8c7522190a48ba33fe87d95dc1c
Kirill Kleymenov <kkleymenov@ozon.ru>
2021-11-04 17:59:08 +0300

    Some basic css

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue';
 import App from '/@/App.vue';
 import router from '/@/router';
 import 'normalize.css';
+import '/@/styles/global.scss';
 
 createApp(App).use(router).mount('#app');
