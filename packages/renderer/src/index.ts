9e9177b619b6d5a6b98ead03933af4954f8da4ee
Kirill Kleymenov <kkleymenov@ozon.ru>
2021-11-14 11:14:25 +0300

    A bit of design

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -3,8 +3,5 @@ import App from '/@/App.vue';
 import router from '/@/router';
 import 'normalize.css';
 import '/@/styles/Global.scss';
-import '/@/styles/Variables.scss';
 
-import FontAwesomeIcon from './fa-icons';
-
-createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
+createApp(App).use(router).mount('#app');
