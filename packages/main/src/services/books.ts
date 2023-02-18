457bbfaea4065de81880603d6b182e3ba6aba9e5
Kirill Kleymenov <k.kelolov@gmail.com>
2023-02-18 15:28:35 +0400

    feat: zod verification

--- packages/main/src/services/books.ts
+++ packages/main/src/services/books.ts
@@ -1,150 +1,45 @@
 import * as matter from 'gray-matter';
 import type { ISavedFile } from './files';
-import settings from './settings';
-import { parse, isValid } from 'date-fns';
+import { z } from 'zod';
 
-export type IDateRead = { started?: string; finished?: string };
+const zDateRead = z.object({ started: z.string().optional(), finished: z.string().optional() });
 
-export type Status = 'read' | 'to-read' | 'reading';
+export type IDateRead = z.infer<typeof zDateRead>;
 
-export type numbersOnlyString = string;
-export interface IBookData {
-  title?: string;
-  author?: string;
-  year?: number;
-  myRating?: number;
-  read?: IDateRead[];
-  tags?: string[];
-  cover?: string;
-  ISBN13?: number;
-}
+const zBookData = z.object({
+  title: z.string().optional(),
+  author: z.string().optional(),
+  year: z.number().optional(),
+  myRating: z.number().optional(),
+  read: z.array(zDateRead).optional(),
+  tags: z.array(z.string()).optional(),
+  cover: z.string().optional(),
+  ISBN13: z.number().optional(),
+});
 
-const numberVerifier = (v: unknown) => {
-  if (!v) return -1;
-  return Number.isNaN(Number(v)) ? -1 : Number(v);
-};
-const stringVerifier = (v: unknown) => (typeof v === 'string' ? v : '');
-
-const stringArrayVerifier = (v: unknown) => {
-  if (!Array.isArray(v)) return [];
-  return Array.from(
-    v.reduce((acc: Set<string>, el: unknown) => {
-      const newS = stringVerifier(el);
-      if (newS) acc.add(newS);
-      return acc;
-    }, new Set()),
-  );
-};
-
-const dateVerifier = (date: unknown, dateFormat: string): string | null => {
-  if (typeof date !== 'string') return null;
-  const parsed = parse(date, dateFormat, new Date());
-  if (!isValid(parsed)) return null;
-  return date;
-};
-
-const ISBNVerifier = (isbn: unknown) => {
-  if (typeof isbn != 'number') return;
-  if (String(isbn).length != 13) return;
-  return isbn;
-};
-
-const readDatesVerifier = (v: unknown) => {
-  if (!Array.isArray(v)) return [];
-  const stngs = settings.getStore();
-  const dateFormat = stngs.dateFormat;
-  return v.reduce((acc, dates) => {
-    const verifiedDate: IDateRead = {};
-
-    const from = dateVerifier(dates.started, dateFormat);
-
-    if (from) {
-      verifiedDate.started = from;
-    }
-    const to = dateVerifier(dates.finished, dateFormat);
-
-    if (to) {
-      verifiedDate.finished = to;
-    }
-
-    if (Object.values(verifiedDate).length > 0) {
-      acc.push(verifiedDate);
-    }
-    return acc;
-  }, []);
-};
-
-type BoodataProp = {
-  key: keyof IBookData;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  verifier: (v: unknown) => any;
-};
-
-const bookDataProps: BoodataProp[] = [
-  {
-    key: 'title',
-    verifier: stringVerifier,
-  },
-  {
-    key: 'author',
-    verifier: stringVerifier,
-  },
-  {
-    key: 'year',
-    verifier: numberVerifier,
-  },
-  {
-    key: 'myRating',
-    verifier: numberVerifier,
-  },
-  {
-    key: 'read',
-    verifier: readDatesVerifier,
-  },
-  {
-    key: 'tags',
-    verifier: stringArrayVerifier,
-  },
-  {
-    key: 'cover',
-    verifier: stringVerifier,
-  },
-  {
-    key: 'ISBN13',
-    verifier: ISBNVerifier,
-  },
-];
+export type IBookData = z.infer<typeof zBookData>;
 
 export const makeBookFile = (path: string, name: string): ISavedFile => {
   const parsed = matter.read(path);
+
+  const res = zBookData.safeParse(parsed.data);
+
+  const toAdd = res.success ? res.data : {};
 
   const file: ISavedFile = {
     content: parsed.content,
     path,
     name,
+    ...toAdd,
   };
-
-  bookDataProps.forEach((prop) => {
-    const data: unknown = parsed.data[prop.key];
-    if (data) {
-      const verified = prop.verifier(data);
-      if (verified) file[prop.key] = prop.verifier(data);
-    }
-  });
 
   return file;
 };
 
 export const makeEncodedBook = (file: ISavedFile): string => {
-  const data: IBookData = {};
+  const res = zBookData.safeParse(file);
 
-  bookDataProps.forEach((prop) => {
-    const property = file[prop.key];
-    if (property) {
-      const verified = prop.verifier(property);
-      if (verified) data[prop.key] = verified;
-    }
-  });
+  const data: IBookData = res.success ? res.data : {};
 
   const result = matter.stringify(file.content || '', data);
 
