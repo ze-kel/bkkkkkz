f95e652b9654a7be5f3489bc25065cde7ed7f7e1
Kirill Kleymenov <k.kelolov@gmail.com>
2024-12-08 23:10:38 +0300

    drop gray matter

--- plugins/node.client.ts
+++
@@ -1,9 +0,0 @@
-import { Buffer } from 'node:buffer';
-import process from 'node:process';
-
-// This is needed to support gray-matter library.
-// Can be deleted after migration to rust frontmatter parsing
-globalThis.Buffer = Buffer;
-globalThis.process = process;
-
-export default defineNuxtPlugin({});
